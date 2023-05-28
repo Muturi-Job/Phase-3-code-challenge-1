2ee46ba89c2b0fa5d9cc0ccc8bbe98a62f4db91a
Job Muturi <muirimuturi17@gmail.com>
2023-05-28 12:31:03 +0300

    Updated author

--- lib/Author.rb
+++ lib/Author.rb
@@ -1,6 +1,24 @@
 class Author
-    attr_accessor :name
+    attr_reader :name
     def initialize(name)
         @name = name
+        @articles = []
+    end
+
+    def articles
+        @articles.dup
+    end
+
+    def magazine
+        @articles.map(&:magazine).uniq
+    end
+
+    def add_article(magazine, title)
+        article = Article.new(self, magazine, title)
+        @article << article
+    end
+
+    def topic_areas
+        magazines.map(&:category).uniq
     end
 end
